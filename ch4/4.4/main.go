fb0e10b9b135ce754cf226bea9a5e2aa927e7cf8
Jason Voigt <joyrexus@gmail.com>
2016-02-11 10:26:45 -0500

    Experiment with in-place reverse technique

--- ch4/4.4/main.go
+++ ch4/4.4/main.go
@@ -1,29 +1,16 @@
 package main
 
-import (
-	// "bufio"
-	"fmt"
-	// "os"
-	// "strconv"
-	// "strings"
-)
-
-// reverse reverses a slice of ints in place.
-func reverse(s []int) {
-	for i, j := 0, len(s)-1; i < j; i, j = i+1, j-1 {
-		s[i], s[j] = s[j], s[i]
-	}
-}
+import "fmt"
 
 func main() {
-	a := []int{0, 1, 2, 3, 4, 5} // array
-	reverse(a)
-	fmt.Println(a) // "[5 4 3 2 1 0]"
-
 	s := []int{0, 1, 2, 3, 4, 5} // array
-	// Rotate s left by two positions.
-	reverse(s[:2])
-	reverse(s[2:])
-	reverse(s)
-	fmt.Println(s) // "[2 3 4 5 0 1]"
+	// Rotate s right by two positions.
+	q := make([]int, len(s))
+	
+	by := 2
+	i := len(s) - by
+	copy(q, s[i:])
+	copy(q[by:], s[:i])
+	copy(s, q)
+	fmt.Println(s) // "[4 5 0 1 2 3]"
 }
